21972edad771bfddfddac7823ca26ea0e18c3dfe
谢恺 <kaixie@caih.com>
2019-08-21 14:52:32 +0800

    完善解密偶尔失败的情况

--- decode_github.js
+++ decode_github.js
@@ -41,14 +41,14 @@ function fileTree(file, loop = true, mode = 'all') {
 
 function question() {
   rl.question(`请选择要使用的功能：\n\n 1.解密当前目录(忽略node_modules)；\n 2.git自动Add\n 3.解除复制限制\n\n 选择：`, async answer => {
-    if(answer == '1') {
+    if (answer == '1') {
       await decode();
       rl.close();
-    }else if(answer == '2') {
+    } else if (answer == '2') {
 
-    }else if(answer == '3') {
+    } else if (answer == '3') {
 
-    }else {
+    } else {
       console.log(``);
       console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~`);
       console.log(`输入错误，请重新输入`);
@@ -58,22 +58,48 @@ function question() {
   });
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function decode() {
   return new Promise(reslove => {
     console.log(`当前目录为：${__dirname}`);
     console.log(`开始解密当前目录...`);
     const fileList = fileTree(__dirname, false);
+    let failFile = [];
+    let failOverList = [];
     fileList.map(item => {
       let itemName = path.basename(item);
-      if(itemName == 'decode.js' || itemName == 'node_modules' || itemName == '.git') return;
+      if (itemName == 'decode.js' || itemName == 'node_modules' || itemName == '.git') return;
       let copyName = `${itemName}_decode`;
       let copyDir = path.join(path.dirname(item), copyName);
-      fs.copySync(item, copyDir);
-      fs.removeSync(item);
-      fs.copySync(copyDir, item);
-      fs.removeSync(copyDir);
+      try {
+        fs.copySync(item, copyDir);
+        fs.removeSync(item);
+        fs.copySync(copyDir, item);
+        fs.removeSync(copyDir);
+      } catch (error) {
+        failFile.push({
+          item,
+          copyDir,
+        });
+      }
     });
     console.log(`目录解密完成！`);
+    // 解密失败时，加入500ms延时，再次尝试解密
+    failFile.length && failFile.map(async file => {
+      const { item, copyDir } = file;
+      try {
+        await sleep(500);
+        fs.copySync(copyDir, item);
+        fs.removeSync(copyDir);
+      } catch (error) {
+        failOverList.push(item);
+      }
+    });
+    // 2次尝试解密失败，抛出错误
+    failOverList.length && console.log(`解密失败文件：${failOverList}`) && console.log(`请手动处理解密失败文件。测试时失败一般会导致copy文件未能覆盖之前的加密文件，请重命名覆盖即可`);;
     reslove();
   });
 }
